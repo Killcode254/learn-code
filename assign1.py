e6d40fc77dc0f42eb7bacb029f999549447c38bd
Antony Muriithi <killcode254@gmail.com>
2022-05-31 12:53:45 +0300

    initial commit

--- assign1.py
+++ assign1.py
@@ -1,7 +1,26 @@
-from unicodedata import name
-
-
 class person ():
-    def __init__(self,name,dob,height,weight)
-    self.name = name
-    self.dob
+    def __init__(self,name,yob,height,weight):
+        self.name = name
+        self.yob = yob
+        self.height = height
+        self.weight = weight
+    def BMI(self) :
+        BMI = self.weight/(self.height*self.height)
+    def age(self):
+        age = 2022-self.yob
+    def details(self):
+        print("my name is", self.name, "and my age is", self.age, "and my BMI is",self.BMI)
+class student (person):
+    def __init__(self,subject,clas):
+        self.subject = subject
+        self.clas = clas
+class teacher (person):
+    def __init__(self,subject,salary):
+        self.subject = subject
+        self.salary = salary
+Moses = person("moses","2010",40,6)
+
+
+
+
+
---
+++ maths.py
@@ -0,0 +1,3 @@
+import math
+print(math.ceil(9.5))
+print(math.pow(5, 2))
